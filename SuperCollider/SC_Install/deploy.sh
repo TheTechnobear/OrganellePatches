9c4c3d648e0b465c159f6fd35e001030f1b1529a
Mark Harris <mharris@technobear.com>
2017-12-17 13:17:20 +0000

    initial untested mec installation

--- SuperCollider/SC_Install/deploy.sh
+++ SuperCollider/SC_Install/deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+export USER_DIR=${USER_DIR:="/usbdrive"}
+# PATCH_DIR=${PATCH_DIR:="/usbdrive/Patches"}
+# FW_DIR=${FW_DIR:="/root"}
+# SCRIPTS_DIR=$FW_DIR/scripts
+
 # should be run from motherhost package installer
 ~/scripts/remount-rw.sh
 
