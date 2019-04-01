2f30a47232d25b765ec2700c1b445e9a7c37e442
Mark Harris <mharris@technobear.com>
2019-04-01 14:00:16 +0200

    add pd49 update

---
+++ PureData/PD49Update/deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+export USER_DIR=${USER_DIR:="/usbdrive"}
+# PATCH_DIR=${PATCH_DIR:="/usbdrive/Patches"}
+# FW_DIR=${FW_DIR:="/root"}
+# SCRIPTS_DIR=$FW_DIR/scripts
+
+# should be run from motherhost package installer
+~/scripts/remount-rw.sh
+
+echo installing pd49 > $USER_DIR/install.log
+
+oscsend localhost 4001 /oled/aux/line/2 s "installing"
+oscsend localhost 4001 /oled/aux/line/3 s "PureData 0.49"
+pacman -U --noconfirm pkgs/*  2>&1 >> $USER_DIR/install.log 
+
+oscsend localhost 4001 /oled/aux/line/2 s "installation"
+oscsend localhost 4001 /oled/aux/line/3 s "complete"
+
+
+echo install done  >>$USER_DIR/install.log
+
+cd ..
+rm -rf $1
+
+exit 1
