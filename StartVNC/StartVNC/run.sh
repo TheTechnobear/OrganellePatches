340532eedd00cb606e3a041cb06e4f6213040ebb
Mark Harris <mharris@technobear.com>
2019-06-19 15:33:45 -0400

    startvnc for org1 and m

--- StartVNC/StartVNC/run.sh
+++ StartVNC/StartVNC/run.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+grep -q 'ID=archarm' /etc/os-release; 
+if [ ! $? -eq 0 ] 
+then 
+	export FW_DIR=${FW_DIR:="~/fw_dir"}
+else 
+	export FW_DIR=${FW_DIR:="/root"}
+fi
+
+
 USER_DIR=${USER_DIR:="/usbdrive"}
 #PATCH_DIR=${PATCH_DIR:="${USER_DIR}/Patches"}
-export FW_DIR=${FW_DIR:="/root"}
 SCRIPTS_DIR=$FW_DIR/scripts
 WORK_DIR=${WORK_DIR:="${USER_DIR}"}
 
@@ -12,11 +20,11 @@ cd $WORK_DIR
 oscsend localhost 4001 /oled/aux/clear i 1
 oscsend localhost 4001 /oled/setscreen i 1
 
-oscsend localhost 4001 /oled/aux/line/2 s "running"
-oscsend localhost 4001 /oled/aux/line/3 s "TigerVNC"
+oscsend localhost 4001 /oled/aux/line/2 s "starting"
+oscsend localhost 4001 /oled/aux/line/3 s "VNC"
 
-cp -R /root/.vnc /tmp
+cp -R ~/.vnc /tmp
 export HOME=/tmp
 
-vncserver
+vncserver -geometry 1920x1080
 
---
+++ StartVNC/deploy.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+export USER_DIR=${USER_DIR:="/usbdrive"}
+# PATCH_DIR=${PATCH_DIR:="/usbdrive/Patches"}
+# FW_DIR=${FW_DIR:="/root"}
+# SCRIPTS_DIR=$FW_DIR/scripts
+SYSTEM_DIR=$USER_DIR/System
+
+echo installing vnc > $USER_DIR/install.log
+
+grep -q 'ID=archarm' /etc/os-release; 
+if [ ! $? -eq 0 ] 
+then 
+	echo platform organelle-m  2>&1 >> $USER_DIR/install.log
+	sudo ~/fw_dir/scripts/remount-rw.sh 2>&1 >> $USER_DIR/install.log
+	oscsend localhost 4001 /oled/aux/line/2 s "configure"
+	cp vnc.m/* /home/music/.vnc 2>&1 >> $USER_DIR/install.log
+	sync
+	sudo ~/fw_dir/scripts/remount-ro.sh
+else 
+	echo platform organelle-1  2>&1 >> $USER_DIR/install.log
+	~/scripts/remount-rw.sh
+	oscsend localhost 4001 /oled/aux/line/2 s "install pkgs"
+	pacman -U --needed --noconfirm pkgs.1/*  2>&1 >> $USER_DIR/install.log 
+	oscsend localhost 4001 /oled/aux/line/2 s "configure"
+	rm -rf /root/.vnc
+	mv vnc.1 /root/.vnc
+	sync
+	~/scripts/remount-ro.sh
+fi
+
+
+
+rm -rf ${SYSTEM_DIR}/TigerVNC  ${SYSTEM_DIR}/StartVNC
+mkdir ${SYSTEM_DIR}
+mv StartVNC ${SYSTEM_DIR}/StartVNC
+
+
+oscsend localhost 4001 /oled/aux/line/2 s "done"
+
+echo install done  >>$USER_DIR/install.log
+
+cd ..
+rm -rf $1
+
+exit 1
