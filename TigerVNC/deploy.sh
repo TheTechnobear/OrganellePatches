e75d76a6517055c4e925dfd50c9ddf5b67f3e1a3
Mark Harris <mharris@technobear.com>
2019-07-06 19:21:55 +0000

    update to make organelle-1 tigervnc

--- TigerVNC/deploy.sh
+++ TigerVNC/deploy.sh
@@ -12,6 +12,8 @@ if [ ! $? -eq 0 ]
 then 
    oscsend localhost 4001 /oled/aux/line/1 s "only valid for"
    oscsend localhost 4001 /oled/aux/line/2 s "organelle-1"
+   cd ..
+   rm -rf $1
    exit 128
 fi
 
